de8be62d1f70425de1dc789f438f9a7fc56391e9
Arij Ben Tej <arijbentej@gmail.com>
2025-03-07 15:25:37 +0100

    build apis

--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -17,8 +17,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # External Packages
-    "rest_framework"
+    "rest_framework",
+    "drf_spectacular",
     # Internal apps
+    "drfecommerce.product",
 ]
 
 MIDDLEWARE = [
@@ -80,4 +82,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Django DRF Ecommerce",
+}
